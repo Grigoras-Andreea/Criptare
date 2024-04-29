9932f770708b4f5acdbc3d3f276ce2990a87f1df
Grigoras-Andreea <andreeasimona180@gmail.com>
2024-04-29 15:29:01 +0300

    finished app

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import random
 
 HEADER = 64
 PORT = 5050
@@ -7,18 +8,80 @@ DISCONNECT_MESSAGE = "!DISCONNECT"
 SERVER = "192.168.100.11"
 ADDR = (SERVER, PORT)
 
+q = 13
+xa = random.randint(1, q - 1)
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
-def send(msg):
+def calculate_a(q):
+    for a in range(1, q):
+        results = []
+        for j in range(1, q):
+            a_to_j = a ** j
+            a_to_j_mod_q = a_to_j % q
+            if a_to_j_mod_q not in results:
+                results.append(a_to_j_mod_q)
+        if len(results) == q - 1:
+            return a
+        
+def calculate_y():
+    #generate random number from 1 to q
+    a = calculate_a(q)
+    
+    ya = (a**xa) % q
+    return ya
+
+def calculate_k(yb):
+    k = (yb**xa) % q
+    return k
+
+def encrypt(msg, k):
+    encrypted_msg = ""
+    for char in msg:
+        encrypted_char = chr(ord(char) + k)
+        encrypted_msg += encrypted_char
+    return encrypted_msg
+
+def send_ya(ya):
+    ya_str = str(ya)
+    ya_msg = ya_str.encode(FORMAT)
+    ya_length = len(ya_msg)
+    send_length = str(ya_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(ya_msg)
+
+def receive_yb():
+    # Receive the length of yb message
+    yb_length = client.recv(HEADER).decode(FORMAT)
+    if yb_length:
+        yb_length = int(yb_length)
+        # Receive yb from the server
+        yb = client.recv(yb_length).decode(FORMAT)
+        return int(yb)
+
+
+def send(msg, yb):
+    msg = encrypt(msg, calculate_k(yb))
     message = msg.encode(FORMAT)
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b' ' * (HEADER - len(send_length))
     client.send(send_length)
     client.send(message)
+    print(client.recv(2048).decode(FORMAT))
     
-send("Hello World!")
-send("Hello Everyone!")
 
-send(DISCONNECT_MESSAGE)
+send_ya(calculate_y())
+yb = receive_yb()
+print(f"Received yb: {yb}")
+print(calculate_k(yb))
+
+while True:
+    msg = input("Enter a message: ")  
+    send(msg, yb)
+    if msg == "":
+        send(DISCONNECT_MESSAGE, yb)
+        break
+    
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
+import random
 import socket
 import threading
-
 
 HEADER = 64
 PORT = 5050
@@ -9,12 +9,62 @@ ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
+q = 13
+xb = random.randint(1, q - 1)
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the server to the address
 server.bind(ADDR)
 
+def calculate_a(q):
+    for a in range(1, q):
+        results = []
+        for j in range(1, q):
+            a_to_j = a ** j
+            a_to_j_mod_q = a_to_j % q
+            if a_to_j_mod_q not in results:
+                results.append(a_to_j_mod_q)
+        if len(results) == q - 1:
+            return a
+
+def calculate_y():
+    #generate random number from 1 to q
+    a = calculate_a(q)
+    
+    yb = (a**xb) % q
+    return yb
+
+def calculate_k(ya):
+    k = (ya**xb) % q
+    return k
+
+def decrypt(msg, k):
+    decrypted_msg = ""
+    for char in msg:
+        decrypted_char = chr(ord(char) - k)
+        decrypted_msg += decrypted_char
+    return decrypted_msg
+
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
+    
+    ya_length = conn.recv(HEADER).decode(FORMAT)
+    if ya_length:
+        ya_length = int(ya_length)
+        ya = conn.recv(ya_length).decode(FORMAT)
+        print(f"[{addr}] ya received: {ya}")
+    
+    # Calculate yb
+    yb = calculate_y()
+    
+    # Send yb to the client
+    yb_str = str(yb)
+    yb_str = yb_str.encode(FORMAT)
+    yb_length = len(yb_str)
+    yb_send_length = str(yb_length).encode(FORMAT)
+    yb_send_length += b' ' * (HEADER - len(yb_send_length))
+    conn.send(yb_send_length)
+    conn.send(yb_str)
     
     connected = True
     while connected:
@@ -25,6 +75,9 @@ def handle_client(conn, addr):
             if msg == DISCONNECT_MESSAGE:
                 connected = False
             print(f"[{addr}] {msg}")
+            msg = decrypt(msg, calculate_k(int(ya)))
+            print(f"[{addr}] Decrypted message: {msg}")
+            conn.send("Message received".encode(FORMAT))
     
     conn.close()
         
